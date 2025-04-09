5ab4a612aa228de0288393ca1ceea87057d6560b
parshuram.m <parshuram.m@accops.com>
2025-04-09 09:00:20 +0530

    Enabling Method level Security to Provide Role based Access to User

--- Security-demo/src/main/java/com/example/Security/demo/GreetingsController.java
+++ Security-demo/src/main/java/com/example/Security/demo/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.example.Security.demo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,16 @@ public class GreetingsController {
     public String sayHello()
     {
         return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint(){
+    return "Hello User";
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndPoint(){
+    return "Hello Admin";
     }
 }
