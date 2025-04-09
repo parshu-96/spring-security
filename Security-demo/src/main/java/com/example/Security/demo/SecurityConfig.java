1376f6891fb5f1787a963747c686ad0e9bd50263
parshuram.m <parshuram.m@accops.com>
2025-04-09 07:01:24 +0530

    Added In Memory User for authentication

--- Security-demo/src/main/java/com/example/Security/demo/SecurityConfig.java
+++ Security-demo/src/main/java/com/example/Security/demo/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,7 +20,22 @@ public class SecurityConfig
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user1= User.withUsername("user1")
+                .password("{noop}password1") //noop is used to save password as plain text
+                .roles("USER")
+                .build();
+        UserDetails admin= User.withUsername("admin")
+                .password("{noop}adminPass") //noop is used to save password as plain text
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1, admin);
     }
 }
